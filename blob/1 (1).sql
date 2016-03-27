e6e64c71233b03483f2e1e00924ba4c6e5574c55
hussainrefaa <hus244@gmail.com>
2016-03-27 21:25:40 +0200

    Added files via upload

---
+++ blob/1 (1).sql
@@ -0,0 +1,49 @@
+-- Author: 	Hussen refaa
+-- creation Date: 	2008
+-- Last Updated: 	0000-00-00
+-- Control Number:	xxxx-xxxx-xxxx-xxxx
+-- Version: 	0.0
+-- Phone : + 4915775148443
+-- Email: hus244@gmail.com
+/* Create a BFILE locator */
+DECLARE
+   web_page BFILE;
+BEGIN
+   --Delete row for Tannery Falls so this example can
+   --be executed multiple times
+   DELETE FROM waterfalls WHERE falls_name='Tannery Falls';
+
+   --Invoke BFILENAME to create a BFILE locator
+   web_page := BFILENAME('BFILE_DATA','Tannery Falls.htm');
+
+   --Save our new locator in the waterfalls table
+   INSERT INTO waterfalls (falls_name, falls_web_page)
+      VALUES ('Tannery Falls',web_page);
+END;
+/
+
+DECLARE
+   web_page BFILE;
+   html RAW(60);
+   amount BINARY_INTEGER := 60;
+   offset INTEGER := 1;
+BEGIN
+   --Retrieve the LOB locater for the web page
+   SELECT falls_web_page
+     INTO web_page
+     FROM waterfalls
+    WHERE falls_name='Tannery Falls';
+
+   --Open the locator, read 60 bytes, and close the locator
+   DBMS_LOB.OPEN(web_page);
+   DBMS_LOB.READ(web_page, amount, offset, html);
+   DBMS_LOB.CLOSE(web_page);
+
+   --Uncomment following line to display results in hex
+   --DBMS_OUTPUT.PUT_LINE(RAWTOHEX(html));
+
+   --Cast RAW results to a character string we can read
+   DBMS_OUTPUT.PUT_LINE(UTL_RAW.CAST_TO_VARCHAR2(html));
+END;
+/
+
---
+++ blob/1 (3).sql
@@ -0,0 +1,38 @@
+-- Author: 	Hussen refaa
+-- creation Date: 	2008
+-- Last Updated: 	0000-00-00
+-- Control Number:	xxxx-xxxx-xxxx-xxxx
+-- Version: 	0.0
+-- Phone : + 4915775148443
+-- Email: hus244@gmail.com
+/* Demonstrates the difference between an empty LOB and a NULL LOB */
+DECLARE
+   directions CLOB;
+BEGIN
+   --Delete any existing rows for 'Munising Falls' so that this
+   --example can be executed multiple times
+   DELETE
+     FROM waterfalls
+    WHERE falls_name='Munising Falls';
+
+   --Insert a new row using EMPTY_CLOB() to create a LOB locator
+   INSERT INTO waterfalls
+             (falls_name,falls_directions)
+      VALUES ('Munising Falls',EMPTY_CLOB());
+
+   --Retrieve the LOB locater created by the previous INSERT statement
+   SELECT falls_directions
+     INTO directions
+     FROM waterfalls
+    WHERE falls_name='Munising Falls';
+
+   IF directions IS NULL THEN
+      DBMS_OUTPUT.PUT_LINE('directions is NULL');
+   ELSE
+      DBMS_OUTPUT.PUT_LINE('directions is not NULL');
+   END IF;
+
+   DBMS_OUTPUT.PUT_LINE('Length = '
+                        || DBMS_LOB.GETLENGTH(directions));
+END;
+/
